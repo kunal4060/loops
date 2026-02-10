25aeb4ecd1390dce478522ecf54e0615ce753993
Kunal Ugale <kunalugale4060@gmail.com>
2026-02-10 19:14:09 +0530

    15

---
+++ phase 1/armstrong.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class armstrong {
+    public static void main(String arg[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+        int original = n;
+        int sum = 0;
+        int digits = String.valueOf(n).length();
+        while(n > 0){
+            int digit = n % 10;
+            sum += Math.pow(digit, digits);
+            n /= 10;
+        }
+        if(original == sum){
+            System.out.println("Armstrong");
+        } else {
+            System.out.println("Not Armstrong");
+        }
+        sc.close();
+    }
+}
