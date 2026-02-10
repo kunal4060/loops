646567517115b0385ce74c63d94464027ed97ca4
Kunal Ugale <kunalugale4060@gmail.com>
2026-02-10 19:12:48 +0530

    10

---
+++ phase 1/factorial.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class factorial {
+    public static void main(String arg[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+        long fact = 1;
+        for(int i = 1; i <= n; i++){
+            fact *= i;
+        }
+        System.out.println("Factorial: " + fact);
+        sc.close();
+    }
+}
