84d36fedd4b80748e3a9b6fd6c7e6e55df0dea54
Kunal Ugale <kunalugale4060@gmail.com>
2026-02-10 19:00:43 +0530

    odd1

---
+++ phase 1/oddno.java
@@ -0,0 +1,12 @@
+
+
+public class oddno {
+    public static void main(String arg[]){
+
+        for(int i = 1 ; i <=100 ; i += 2){
+            System.out.println(i);
+        }
+      
+    }
+    
+}
