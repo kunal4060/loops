0f5ce4ca580aa3aeaa5ede0d48d3beecebed8833
Kunal Ugale <kunalugale4060@gmail.com>
2026-02-10 19:44:09 +0530

    yes babe nothiung

--- phase 1/factors.java
+++ phase 1/factors.java
@@ -8,7 +8,9 @@ public class factors {
         for(int i = 1; i <= n; i++){
             if(n % i == 0){
                 System.out.print(i + " ");
+                
             }
+            System.out.println(n % i);
         }
         System.out.println();
         sc.close();
