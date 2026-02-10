70bf018b214ab8645c4f09e11c66a011788f5bff
Kunal Ugale <kunalugale4060@gmail.com>
2026-02-10 18:59:40 +0530

    even2

--- phase 1/even.java
+++ phase 1/even.java
@@ -3,8 +3,13 @@
 public class even {
     public static void main(String arg[]){
 
-        for(int i = 2 ; i <=10 ; i += 2){
-            System.out.println(i);
+        // for(int i = 2 ; i <=100 ; i += 2){
+        //     System.out.println(i);
+        // }
+        for(int i = 1 ; i <=100 ; i ++){
+            if(i % 2 == 0){
+                System.out.println(i);
+            }
         }
     }
     
